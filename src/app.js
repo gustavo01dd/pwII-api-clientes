6d229513d0a60e026362769a386ab1a3f3be9553
Felipissimo <falmeidalima7@gmail.com>
2026-04-29 22:19:30 -0300

    Atualizações e adiçoes na Aula de PW2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const clientesRoutes = require("./routes/clientesRoutes");
 
 const app =  express();
 
@@ -6,3 +7,29 @@ const PORT = process.env.Port || 3000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.json({
+        mensagem: "API de clientes",
+        versao: "1.0.0",
+        endpoints: {
+            listarTodos: "GET /clientes",
+            buscarPorId: "GET /clientes/:id,"            
+        }
+    })
+})
+
+
+app.use("/clientes", clientesRoutes);
+
+app.use((req, res) =>{
+    res.status(404).json({
+        sucesso: false, 
+        mensagem: "Rota Não Encontrada",
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`servidor rodando na porta ${PORT}`)
+})
+
+module.exports = app;
